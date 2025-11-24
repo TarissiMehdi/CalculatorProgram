8b5db8eaf638ca6087d57b8e1f8315b2c30e96ff
Trickster <Mehdi.Tarissi@emsi-edu.ma>
2025-11-24 13:44:51 +0100

    Calculator operates correctly with error handling on the console

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # Output
-<img width="550" height="699" alt="image" src="https://github.com/user-attachments/assets/2ba67f63-d785-4a62-a5b9-8f9db630813c" />
 
 
--- index.js
+++ index.js
@@ -1,41 +1,19 @@
+// Calculator Program
 const display = document.getElementById("display");
-let timer = null;
-let startTime = 0;
-let elapsedTime = 0;
-let isRunning = false;
+function appendToDisplay(input){
+    display.value+=input;
+}
 
-function start() {
-    if(!isRunning){
-        isRunning = true;
-        startTime = Date.now() - elapsedTime;
-        timer = setInterval(update, 10);
+function calculate(){
+    try{
+        display.value=eval(display.value);
     }
-}
-
-function stop() {
-    if(isRunning){
-        clearInterval(timer); // cancels a timed repeated action which was being called by setInterval
-        elapsedTime=Date.now()-startTime;
-        isRunning=false;
+    catch(error){
+        console.error("Syntax error")
     }
+    
 }
 
-function reset() {
-    clearInterval(timer); // cancels a timed repeated action which was being called by setInterval
-    startTime=0;
-    elapsedTime=0;
-    isRunning=false;
-    display.textContent="00:00:00:00";
+function clearDisplay(){
+    display.value="";
 }
-
-function update(){
-    const currentTime = Date.now();
-    elapsedTime = currentTime - startTime;
-
-    let hours = String(Math.floor(elapsedTime/(1000*60*60))).padStart(2,"0");
-    let minutes = String(Math.floor(elapsedTime/(1000*60)%60)).padStart(2,"0");
-    let seconds = String(Math.floor(elapsedTime/1000%60)).padStart(2,"0");
-    let milliseconds = String(Math.floor(elapsedTime%1000/10)).padStart(2,"0");
-
-    display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
-} 
